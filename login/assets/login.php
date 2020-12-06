7c513176017f174c80e91f7dec85decf6f1b1226
MORIMOTO520212 <kanten520212@gmail.com>
2020-12-06 22:48:40 +0900

    2020.12.06

--- login/assets/login.php
+++ login/assets/login.php
@@ -1,7 +1,39 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '');
-$db_selected = mysqli_select_db('test', $link);
-if (!$db_selected){
-    die('データベース選択失敗です。'.mysqli_error());
+// MySQL オブジェクト指向
+
+// 接続する
+$host = "localhost";
+$user = "root";
+$pass = "";
+$DB = "test";
+$mysqli = new mysqli($host, $user, $pass, $DB);
+
+/* 接続状況をチェック */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
+
+// データベースを選択する
+$mysqli->select_db("tb1");
+
+// tb1のデータを取得するクエリを実行
+$select = " SELECT ";
+$colum  = " * ";
+$from   = " FROM ";
+$table  = " tb1 ";
+$query = $select.$colum.$from.$table;
+
+$result = $mysqli->query($query);
+if(!$result){
+    echo "クエリ実行失敗";
+}
+
+// 表示
+while($row = $result->fetch_row()){
+    var_dump($row);
+}
+
+// 終了
+$mysqli->close();
 ?>
